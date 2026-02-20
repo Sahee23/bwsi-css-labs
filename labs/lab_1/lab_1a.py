26808233f7f84fb37b3a1855ea810bc1c11d4891
Sahee23 <saha.eeshan@gmail.com>
2026-02-20 16:46:31 -0500

    Print a greeting

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Eeshan Saha" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print(f"{name}, how do you do?")
 
 if __name__ == "__main__":
     main()
